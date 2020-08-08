bb9136b3247a2de554d02d07119829b90763eb94
Mong Dao <mongdaovan86@yahoo.com>
2020-08-08 10:33:06 +0700

    update sort id

--- src/models/search/ArticleSearch.php
+++ src/models/search/ArticleSearch.php
@@ -46,6 +46,7 @@ class ArticleSearch extends Article
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort' => ['defaultOrder' => ['id' => SORT_DESC]]
         ]);
 
         $this->load($params);
